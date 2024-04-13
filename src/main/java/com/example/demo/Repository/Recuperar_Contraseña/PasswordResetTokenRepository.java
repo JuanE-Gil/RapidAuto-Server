6fd95686b5fcde409b6e2ebab50ee4165840035b
LuisRiveraBan <159965933+LuisRiveraBan@users.noreply.github.com>
2024-04-12 19:28:29 -0500

    Eliminar

--- src/main/java/com/example/demo/Repository/Recuperar_Contraseña/PasswordResetTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository.Recuperar_Contraseña;
-
-import com.example.demo.Model.Recuperar_Contraseña.PasswordResetToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Integer> {
-    PasswordResetToken findByToken(String token);
-}
